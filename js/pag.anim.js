c7efa3f7d09b7f9595a55291f666d884478acc8e
Daniel Nava Revolledo <daniel_navrev@hotmail.com>
2020-10-20 05:10:26 -0500

    Guardar Cambios

--- js/pag.anim.js
+++ js/pag.anim.js
@@ -6,9 +6,27 @@ window.onscroll = function() {
     } else {
         document.getElementById('mainNavbar').style.top = '-56px';
     }
+    console.log("Actual deslice +1: " + (actual_deslice + 1));
+
+    if (actual_deslice >= 546) {
+        console.log("se cumple");
+        $('#mainNavbar').removeClass('navbar-light').addClass('bg-primary');
+        //$('.text-text-navigationitem').removeClass('nav-item').addClass('text-navitem-dark');
+
+    } else {
+        //$('.text-navitem-dark').removeClass('text-navitem-dark').addClass('nav-item');
+        $('#mainNavbar').removeClass('bg-primary').addClass('navbar-light');
+    }
+    console.log(actual_deslice);
     mainPlace = actual_deslice;
 }
 
+<!-- Menu Toggle Script -->
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
 
 $('#dmenu').hover(function() {
     $('#navbarDropdown').trigger('click');
